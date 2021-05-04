e6447970f2dcbb3e42e94cc0bd5ed751dd1a0f71
Liana <lianayeghoyan96@gmail.com>
2021-05-05 00:13:09 +0430

    professor

--- src/professor/Professor.java
+++ src/professor/Professor.java
@@ -1,4 +1,35 @@
 package professor;
 
 public class Professor implements ProfessorInterface {
-}
+        private String title;
+        private String department;
+        private Vector teaches;
+
+        public Professor(String name, String ssn, String title, String dept) {
+
+            super(name, ssn);
+
+            setTitle(title);
+            setDepartment(dept);
+
+            teaches = new Vector();
+        }
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setDepartment(String dept) {
+            department = dept;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+
+    }
+
+
